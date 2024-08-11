f1d3cec7dce290644e76662a31a50aaf8a09f5c8
fadhil9 <fadhilmuhamad272@gmail.com>
2024-08-11 15:49:14 +0700

    update

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Test struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func setupRouter() *gin.Engine {
 	// deklare framework gin
 	r := gin.Default()
@@ -16,9 +21,19 @@ func setupRouter() *gin.Engine {
 			"status": "succes",
 			"value":  "hello  fadhil ganteng",
 		}) //gin.H untuk generate json bisa juga pake map : ctx.JSON(http.StatusOK,map[string]interface{}{"blabla"}
-
 	})
 
+	//digrupin "v1" masukin ke var v1
+	v1 := r.Group("v1")
+	// cara gunain grupnya
+	v1.GET("/user/:name", func(ctx *gin.Context) {
+		param := ctx.Param("name")
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "succes",
+			"value":  param,
+		})
+	})
+	v1.POST("user")
 	return r
 }
 
